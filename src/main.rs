d5a8d41ee6e867fd8b4feb972b63ec0c63a5f4f2
Pawel Perek <pawel@perek.pro>
2021-04-18 21:29:30 +0300

    Create types for assembler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#[derive(Copy, Clone)]
+struct Address(u8);
+
+#[derive(Copy, Clone)]
+enum Target {
+    Register(u8),
+    Immediate(u8)
+} 
+
+#[derive(Copy, Clone)]
+enum Instructions {
+    J(Address),
+    ADD(Target),
+    MUL(Target),
+    BIGMUL,
+    LD(Address),
+    ST(Address)
+}
+
 fn main() {
     println!("Hello, world!");
 }
